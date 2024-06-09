71590ad571bdb61f342690868666c107de46050b
Ali-nasser1 <popa0251@gmail.com>
2024-06-09 20:58:21 +0300

    adding client side validation using JQuery

--- Project.PL/obj/Debug/net5.0/Project.PL.AssemblyInfo.cs
+++ Project.PL/obj/Debug/net5.0/Project.PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e719a26799fc201632b88a750c1d9f934bdfc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e180e4506305e3d1e35f4daa6765e633beb25c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
